7e9b3a7c5f5b15310e63e11a128418d3568ca634
Slippor <slippform@gmail.com>
2012-08-30 23:14:01 +0800

    Add a test project.

--- Slippor.Rent.RentMonitor/Slippor.Common/WebWorker/IWebWorker.cs
+++ Slippor.Rent.RentMonitor/Slippor.Common/WebWorker/IWebWorker.cs
@@ -3,38 +3,58 @@ using System.Net;
 
 namespace Slippor.Utility.WebWorker
 {
-    public interface IWebWorker
-    {
-        /// <summary>
-        /// 登录
-        /// </summary>
-        /// <param name="username">用户名</param>
-        /// <param name="password">密码</param>
-        /// <returns>返回的Cookie</returns>
-        String Login(String username, String password);
+	/// <summary>
+	/// 网站工具接口
+	/// </summary>
+	public interface IWebWorker
+	{
+		/// <summary>
+		/// 登录
+		/// </summary>
+		/// <param name="username">用户名</param>
+		/// <param name="password">密码</param>
+		/// <returns>返回的Cookie</returns>
+		String Login(String username, String password);
 
-        /// <summary>
-        /// 登录
-        /// </summary>
-        /// <param name="loginData">登录信息</param>
-        /// <returns>返回的Cookie</returns>
-        String Login(byte[]loginData);
+		/// <summary>
+		/// 登录
+		/// </summary>
+		/// <param name="loginData">登录信息</param>
+		/// <returns>返回的Cookie</returns>
+		String Login(byte[] loginData);
 
-        /// <summary>
-        /// 获取响应
-        /// </summary>
-        /// <param name="url">目标Url</param>
-        /// <param name="postBytes">发送数据</param>
-        /// <param name="cookie">Cookie</param>
-        /// <param name="getStream">是否获取内容</param>
-        /// <param name="method">获取方式</param>
-        /// <returns>响应内容</returns>
-        String AchieveResponse(String url, byte[] postBytes, object cookie, bool getStream, RequestMethod method);
-    }
+		/// <summary>
+		/// 请求Url，但不获取响应内容
+		/// </summary>
+		/// <param name="url"></param>
+		/// <param name="postBytes"></param>
+		/// <param name="cookie"></param>
+		/// <param name="method"></param>
+		/// <returns></returns>
+		bool Ping(String url, byte[] postBytes, object cookie, RequestMethod method);
 
-    public enum RequestMethod
-    {
-        Get,
-        Post
-    }
+		/// <summary>
+		/// 获取响应
+		/// </summary>
+		/// <param name="url">目标Url</param>
+		/// <param name="postBytes">发送数据</param>
+		/// <param name="cookie">Cookie</param>
+		/// <param name="getResponse">是否获取响应内容</param>
+		/// <param name="method">获取方式</param>
+		/// <returns>响应内容</returns>
+		String AchieveResponse(String url, byte[] postBytes, object cookie, bool getResponse, RequestMethod method);
+	}
+
+	public enum RequestMethod
+	{
+		/// <summary>
+		/// Get方法
+		/// </summary>
+		Get,
+
+		/// <summary>
+		/// Post方法
+		/// </summary>
+		Post
+	}
 }
--- Slippor.Rent.RentMonitor/Slippor.Rent.Common/Infomation/RentInfomation.cs
+++ Slippor.Rent.RentMonitor/Slippor.Rent.Common/Infomation/RentInfomation.cs
@@ -5,54 +5,81 @@ using System.Text;
 
 namespace Slippor.Rent.Common
 {
-    public class RentInfomation
-    {
-        public String Lessor
-        {
-            get;
-            set;
-        }
+	/// <summary>
+	/// 租赁信息
+	/// </summary>
+	public class RentInfomation
+	{
+		/// <summary>
+		/// 出租人
+		/// </summary>
+		public String Lessor
+		{
+			get;
+			set;
+		}
 
-        public String PhoneNumber
-        {
-            get;
-            set;
-        }
+		/// <summary>
+		/// 电话号码
+		/// </summary>
+		public String PhoneNumber
+		{
+			get;
+			set;
+		}
 
-        public String Title
-        {
-            get;
-            set;
-        }
+		/// <summary>
+		/// 标题
+		/// </summary>
+		public String Title
+		{
+			get;
+			set;
+		}
 
-        public String Description
-        {
-            get;
-            set;
-        }
+		/// <summary>
+		/// 描述
+		/// </summary>
+		public String Description
+		{
+			get;
+			set;
+		}
 
-        public double Cost
-        {
-            get;
-            set;
-        }
+		/// <summary>
+		/// 花费
+		/// </summary>
+		public double Cost
+		{
+			get;
+			set;
+		}
 
-        public String Address
-        {
-            get;
-            set;
-        }
+		/// <summary>
+		/// 地址
+		/// </summary>
+		public String Address
+		{
+			get;
+			set;
+		}
 
-        public String Url
-        {
-            get;
-            set;
-        }
+		/// <summary>
+		/// 链接
+		/// </summary>
+		public String Url
+		{
+			get;
+			set;
+		}
 
-        public DateTime PublishDate
-        {
-            get;
-            set;
-        }
-    }
+		/// <summary>
+		/// 发布时间
+		/// </summary>
+		public DateTime PublishDate
+		{
+			get;
+			set;
+		}
+	}
 }
